6755c88e89178ef769304498158469c5495adcee
sank20144 <replacefakes@gmail.com>
2020-05-06 01:43:41 +0530

    Update index.php

--- FBPoster/index.php
+++ FBPoster/index.php
@@ -11,7 +11,8 @@ require __DIR__ . '/config.php';
 
 
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['manage_pages', 'pages_show_list', 'publish_pages']; // optional
+$permissions = ['manage_pages', 'pages_show_list', 'publish_pages']; // previous permission
+$permissions = ['pages_show_list', 'pages_read_engagement', 'pages_manage_posts'];
 $loginUrl = $helper->getLoginUrl('http://localhost/FBPoster/login-callback.php', $permissions);
 
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
